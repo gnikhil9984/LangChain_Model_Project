518f0c601d2844717a5e47dc6daa1a8e20e44269
gnikhil9984 <nikhilgupta9984@gmail.com>
2026-08-19 11:30:27 +0530

    runnable passthrough

---
+++ Runnables/runnable_passthrough.py
@@ -0,0 +1,39 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableLambda
+import os
+
+load_dotenv()
+
+LLM = HuggingFaceEndpoint(
+    repo_id= "meta-llama/Llama-3.1-8B-Instruct",
+    task= "text-generartion",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+)
+
+model = ChatHuggingFace(llm = LLM)
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+print(final_chain.invoke({'topic':'cricket'}))
